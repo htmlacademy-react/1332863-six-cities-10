57242cc7a3f64e1cb26c21fa9e2b5582a45d9ca3
Ciberian <isi04047891@gmail.com>
2022-07-10 12:45:19 +0500

    Добавляет компонент пустой страницы с Избранным

--- project/src/components/locations-item/locations-item.tsx
+++ project/src/components/locations-item/locations-item.tsx
@@ -3,7 +3,7 @@ type LocationProps = {
   isActive: boolean;
 }
 
-function LocationItem({locationName, isActive = false}: LocationProps): JSX.Element {
+function LocationItem({locationName, isActive}: LocationProps): JSX.Element {
   return (
     <li className="locations__item">
       <a className={`locations__item-link tabs__item ${isActive ? 'tabs__item--active' : ''}`} href="/#">
