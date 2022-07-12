ba3e5f3720cccc0cfef679a4a4682336b9621117
Ciberian <isi04047891@gmail.com>
2022-07-12 18:14:26 +0500

    Добавляет компонент приватного маршрута

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,17 +4,18 @@ import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import OfferPage from '../../pages/offer-page/offer-page';
 import PageNotFound from '../../pages/page-not-found/page-not-found';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route index element={<MainPage />} />
-          <Route path="login.html" element={<LoginPage />} />
-          <Route path="favorites.html" element={<FavoritesPage />} />
-          <Route path="/offer.html" element={<OfferPage propertyRating={40} reviewRating={80} />} />
-          <Route path="*" element={<PageNotFound />} />
+        <Route path={AppRoute.Root}>
+          <Route index element={<MainPage/>}/>
+          <Route path={AppRoute.Login} element={<LoginPage/>}/>
+          <Route path={AppRoute.Favorites} element={<FavoritesPage/>}/>
+          <Route path={AppRoute.Offer} element={<OfferPage propertyRating={40} reviewRating={80}/>}/>
+          <Route path="*" element={<PageNotFound/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ project/src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Root = '/',
+  Login = 'login.html',
+  Favorites = 'favorites.html',
+  Offer = 'offer.html'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
