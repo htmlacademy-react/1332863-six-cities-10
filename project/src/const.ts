3b4330a2ecebefe8b58ff712727f651e031c33f6
Ciberian <isi04047891@gmail.com>
2022-07-12 22:56:23 +0500

    Исправляет ошибку в приватной маршрутизации

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,8 +1,8 @@
 export enum AppRoute {
   Root = '/',
-  Login = 'login',
-  Favorites = 'favorites',
-  Offer = 'offer'
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer'
 }
 
 export enum AuthorizationStatus {
