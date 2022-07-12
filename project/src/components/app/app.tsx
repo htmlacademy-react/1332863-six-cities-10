1559bb0359f81215705254d7a0b18473f81b5407
Ciberian <isi04047891@gmail.com>
2022-07-12 17:53:44 +0500

    Добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,24 @@
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
-  return <MainPage/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<MainPage />} />
+          <Route path="login.html" element={<LoginPage />} />
+          <Route path="favorites.html" element={<FavoritesPage />} />
+          <Route path="/offer.html" element={<OfferPage propertyRating={40} reviewRating={80} />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
