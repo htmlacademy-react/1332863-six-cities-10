ce34150f52eea48d57f02f97930394d600c868dd
Ciberian <isi04047891@gmail.com>
2022-07-19 15:20:36 +0500

    Добавляет обработчик на ревью-форму

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,15 +1,25 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, FormEvent } from 'react';
 import ReviewRating from '../review-rating/review-rating';
 
 const MAX_REVIEW_RATING = 5;
 const MIN_REVIEW_RATING = 1;
+const MIN_REVIEW_SIMBOLS = 50;
+const MAX_REVIEW_SIMBOLS = 300;
 
 function ReviewForm():JSX.Element {
   const [formState, setFormState] = useState({
     rating: 0,
-    review: ''
+    review: '',
+  });
+
+  function isReviewLongEnough(): boolean {
+    const reviewLength = formState.review.trim().length;
+    return reviewLength >= MIN_REVIEW_SIMBOLS && reviewLength <= MAX_REVIEW_SIMBOLS;
   }
-  );
+
+  function onSendReview(rating: number, review: string) {
+    throw new Error('Function \'onSendReview\' isn\'t implemented.');
+  }
 
   const formStateChangeHandler = ({target}: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
     const {name, value} = target;
@@ -27,7 +37,13 @@ function ReviewForm():JSX.Element {
   };
 
   return (
-    <form className="reviews__form form" action="#" method="post">
+    <form className="reviews__form form" action="#" method="post" onSubmit={(evt: FormEvent<HTMLFormElement>) => {
+      evt.preventDefault();
+      if (isReviewLongEnough()) {
+        onSendReview(formState.rating, formState.review);
+      }
+    }}
+    >
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
         {getReviewRating()}
@@ -37,7 +53,7 @@ function ReviewForm():JSX.Element {
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={false}>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={isReviewLongEnough()}>Submit</button>
       </div>
     </form>
   );
