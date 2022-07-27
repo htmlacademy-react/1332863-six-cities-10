6ff988939625a6322085f3c4d7baf94d5a83bfb0
Ciberian <isi04047891@gmail.com>
2022-07-27 23:42:08 +0500

    Меняет тип маркеров на карте

--- project/src/const.ts
+++ project/src/const.ts
@@ -20,6 +20,6 @@ export const CITIES = [
   'Dusseldorf'
 ];
 
-export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+export const URL_MARKER_DEFAULT = 'img/pin.svg';
 
-export const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export const URL_MARKER_CURRENT = 'img/pin-active.svg';
