715fcd6a96cfb4708571cd5fd4f676e9546d510f
Ciberian <isi04047891@gmail.com>
2022-07-30 21:31:51 +0500

    Добавляет обработку ошибок взаимодействия с сервером

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
 import { Provider } from 'react-redux';
+import App from './components/app/app';
+import ErrorMessage from './components/error-message/error-message';
+import { checkAuthAction } from './store/api-actions';
 import { store } from './store';
-import { loadOffers } from './store/action';
 
-store.dispatch(loadOffers);
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -14,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
+      <ErrorMessage />
       <App />
     </Provider>
   </React.StrictMode>
